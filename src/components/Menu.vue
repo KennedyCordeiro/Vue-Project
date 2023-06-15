a59a49546a31f552e919856b4df951ff442284ee
Kennedy Rocha <kennedy.rocha@wings4business.com.br>
2023-06-15 12:34:10 -0300

    adição de arquivos

---
+++ src/components/Menu.vue
@@ -0,0 +1,8 @@
+
+<template>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Login">Login</RouterLink>
+    </nav>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref} from 'vue';
+
+function login() {
+    console.log('teste login', email.value, password.value);
+}
+
+const email = ref('');
+const password = ref('');
+
+</script>
+
+<template>
+    {{email}}
+
+    <form action="" @submit.prevent="login">
+        <input type="text" placeholder="Email" v-model="email">
+        <input type="text" placeholder="Password" v-model="password">
+        <button type="submit">Login</button>
+    </form>
+</template>
+
+<style></style>
